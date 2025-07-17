1a4fd8a68fb2b89c3c0a9748f099ccfa32201441
V0xa1tor <v0xa1tor.emz@gmail.com>
2025-07-17 20:13:25 -0300

    navigateFallback: /index.html

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,7 +66,7 @@ export default defineNuxtConfig({
     },
     registerType: 'autoUpdate',
     workbox: {
-      navigateFallback: '/',
+      navigateFallback: '/index.html',
       globPatterns: ['**/*']
     }
   },
