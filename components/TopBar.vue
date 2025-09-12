2ba6afd6c217958cbb8a70a7af7a9f594e05c815
V0xa1tor <v0xa1tor.emz@gmail.com>
2025-09-11 23:07:25 -0300

    Breadcrumb center overflow

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -32,11 +32,13 @@ const breadcrumbItems = computed(() => {
       <button class="btn p-1 fs-4" @click="$router.back()"><i class="bi bi-arrow-left"></i></button>
       <button class="btn p-1 fs-4" @click="$router.forward()"><i class="bi bi-arrow-right"></i></button>
     </div>
-    <ol class="breadcrumb align-items-center flex-nowrap flex-grow-1 justify-content-center m-0 overflow-auto">
-      <li v-for="item in breadcrumbItems" class="breadcrumb-item text-nowrap">
-        <NuxtLink class="text-decoration-none" :class="{ 'text-body': !item.path && item.name, 'text-secondary': !item.name }" :to="item.path">{{ item.name || 'Sem título' }}</NuxtLink>
-      </li>
-    </ol>
+    <div class="flex-grow-1 hstack overflow-auto">
+      <ol class="breadcrumb flex-nowrap m-auto">
+        <li v-for="item in breadcrumbItems" class="breadcrumb-item text-nowrap">
+          <NuxtLink class="text-decoration-none" :class="{ 'text-body': !item.path && item.name, 'text-secondary': !item.name }" :to="item.path">{{ item.name || 'Sem título' }}</NuxtLink>
+        </li>
+      </ol>
+    </div>
     <div class="p-2 hstack align-items-center">
       <button class="btn p-1 fs-4"><i class="bi bi-three-dots-vertical"></i></button>
     </div>
