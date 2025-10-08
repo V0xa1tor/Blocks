5507fd396dedb862b9e88d47d2fbd731ee867994
V0xa1tor <v0xa1tor.emz@gmail.com>
2025-10-07 21:40:40 -0300

    Action menu dividers

--- app/components/ActionMenu.vue
+++ app/components/ActionMenu.vue
@@ -17,17 +17,20 @@ function toggleOffcanvas() {
 
 <template>
   <div id="action-menu"
-    class="d-flex flex-md-column border-end flex-grow-0 p-2 gap-3 overflow-auto bg-body-tertiary"
+    class="d-flex flex-md-column justify-content-evenly justify-content-md-start border-end flex-grow-0 p-2 gap-3 overflow-auto bg-body-tertiary"
   >
     <button class="btn p-1 fs-4" @click="toggleOffcanvas"><i class="bi bi-list"></i></button>
-    <div v-for="item in actionMenu.items" class="w-100">
+    <div v-for="item in actionMenu.items">
       <button v-if="item.type == 'action'"
         class="btn fs-4 p-1"
         @click="navigateTo(item.action.to)"
       >
         <i :class="`bi ${item.icon}`"></i>
       </button>
-      <hr v-if="item.type == 'divider'" class="my-0 w-100" style="color: var(--bs-border-color); opacity: 1;">
+      <div v-if="item.type == 'divider'" class="h-100">
+        <hr class="my-0 w-100 d-none d-md-block" style="color: var(--bs-border-color); opacity: 1;">
+        <div class="vr d-md-none h-100"></div>
+      </div>
     </div>
   </div>
 </template>
